da6e3aa0daed8d51a7e7e7efe4afb1309da89ff3
SabrinaCat <Sabrina.glow22@gmail.com>
2020-08-05 16:09:04 -0500

    calendar.js

---
+++ calendar.js
@@ -0,0 +1,10 @@
+// for nodejs
+const moment = require('moment');
+const dateFormat = 'YYYY-MM-dd';
+const numberOfDays = 7;
+
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const shortMonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const shortDays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
