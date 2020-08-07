e5b11682d5c95f66da891efc623cbceaf2b0ae45
Evan Boswood <evan@boswood.net>
2020-08-06 22:56:57 -0500

    appended user's pets to request

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -1,4 +1,4 @@
-const { User, AuthToken } = require('../models');
+const { User, AuthToken, Pet } = require('../models');
 
 module.exports = async function(req, res, next){
 	// Set token from existing cookie
@@ -11,6 +11,12 @@ module.exports = async function(req, res, next){
 		// If token found, attach associated object to pass to routes
 		if(authToken){
 			req.user = authToken.User;
+			const pets = await Pet.findAll({ where: {
+				userId: authToken.User.id
+				}
+			});
+			
+			req.pets = pets;
 		}
 	}
 	next();
