7291764bd1da180242d05ab0d105fbecc87d08c9
Evan Boswood <evan@boswood.net>
2020-08-06 22:56:47 -0500

    added get function

--- server/controllers/pet-controller.js
+++ server/controllers/pet-controller.js
@@ -1,5 +1,6 @@
 // Add Dependencies
 const express = require('express');
+// Placeholder
 
 const router = express.Router();
 
@@ -10,6 +11,36 @@ const { Pet } = require('../models');
 // Routes ------------------------------------------------------------
 //
 
-router.get('/api/pets:id', function(req, res){
-	
+router.get('/', async function(req, res){
+	// Check if user is logged in, if not return 400 Not Authorised
+	if(!req.user){
+		res.json({
+			code: '400',
+			message: 'No user logged in'
+		});
+		return;
+	}
+
+	console.log(req.user);
+
+	// Set current user object
+	const thisUser = req.user;
+
+	const petArr = await Pet.findAll({
+		where: {
+			userId: thisUser.id
+		}
+	});
+
+	console.log(petArr);
+
+	if(petArr.length == 0){
+		res.render('error', {
+			code: 404,
+			message: 'No pets found for this user!'
+		});
+	}
 });
+
+
+module.exports = router;
