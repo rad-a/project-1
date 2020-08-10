51901e19142e4a7b8e47454ce555089684db0031
Evan Boswood <evan@boswood.net>
2020-08-10 17:00:48 -0500

    updated index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,11 +2,11 @@ const userController = require('./user-controller');
 const petController = require('./pet-controller');
 const msgController = require('./msg-controller');
 const viewController = require('./view-controller');
-const eventsController = require('./events-controller');
+const eventController = require('./event-controller');
 module.exports = {
 	userController,
 	petController,
 	viewController,
 	msgController,
-	eventsController
+	eventController
 }
