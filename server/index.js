69c6700b763b4eb965be74d3ee6663caf00faa89
rad-a <11688089+rad-a@users.noreply.github.com>
2020-08-15 16:42:41 -0500

    Added profileImg to render list on in routes

--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
 // Add Dependencies and Required Consts
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 //changed bottom from require('./models/index')
-const db = require('./models/index');
-const authMiddleware = require('./middleware/auth-middleware');
-const cors = require('cors');
-const { userController, petController } = require('./controllers');
-const { sequelize } = require('./models/index');
-const clientDir = path.join(__dirname, '../client');
-const { Op } = require('sequelize');
-
+const db = require("./models/index");
+const authMiddleware = require("./middleware/auth-middleware");
+const cors = require("cors");
+const { userController, petController } = require("./controllers");
+const { sequelize } = require("./models/index");
+const clientDir = path.join(__dirname, "../client");
+const { Op } = require("sequelize");
 
 // Express App Setup
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Socket.io Setup
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 // Express JSON Middleware Setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,8 +29,8 @@ app.use(bodyParser.json());
 app.use(express.static(clientDir));
 
 // Pug Engine Setup
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine','pug');
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "pug");
 
 // Express Cookie Middleware Setup
 app.use(cookieParser());
@@ -41,169 +39,167 @@ app.use(cookieParser());
 app.use(authMiddleware);
 
 // Custom Routing
-app.use('/user', userController);
-app.use('/pets', petController);
+app.use("/user", userController);
+app.use("/pets", petController);
 
 // CORS Setup
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 //
 // Routes
 //
-app.get('/', function(req, res){
-	if(!req.user){
-		res.render('login');
-	} else{
-		res.redirect('/home');
-	}
+app.get("/", function (req, res) {
+  if (!req.user) {
+    res.render("login");
+  } else {
+    res.redirect("/home");
+  }
 });
 
 // Home page
-app.get('/home', async (req, res) => {
-	let username = req.user.username;
-	let thisID = req.user.id;
+app.get("/home", async (req, res) => {
+  let username = req.user.username;
+  let thisID = req.user.id;
+  let profileImg = req.user.link;
 
+  let allUsers = await db.User.findAll({
+    where: {
+      id: {
+        [Op.ne]: thisID,
+      },
+    },
+  });
 
-	let allUsers = await db.User.findAll({
-		where: {
-			id: {
-				[Op.ne]: thisID
-			}
-		}
-	});
-	
-
-	
-	
-	if(req.user){
-		if(req.pets.length == 0){
-			res.render('petAdd',{
-				welcomeMessage: `Hi ${username}!`,
-				pageMsg: "Looks like you dont have any pets on your account yet, let's add some!"
-			});
-		} else {
-			res.render('home', { 
-				pets: req.pets,
-				allUsers: allUsers,
-				user: req.user,
-				numPets: req.user.numPets
-			});
-		}
-	} else {
-		res.render('error');
-	}
-	
+  if (req.user) {
+    if (req.pets.length == 0) {
+      res.render("petAdd", {
+        welcomeMessage: `Hi ${username}!`,
+        pageMsg:
+          "Looks like you dont have any pets on your account yet, let's add some!",
+      });
+    } else {
+      res.render("home", {
+        pets: req.pets,
+        allUsers: allUsers,
+        user: req.user,
+        numPets: req.user.numPets,
+        profileImg: profileImg,
+      });
+    }
+  } else {
+    res.render("error");
+  }
 });
 
+app.get("/messages", (req, res) => {
+  let allUsers = db.User.findAll();
+  let username = req.user.username;
+  let userID = req.user.id;
+  let profileImg = req.user.link;
 
-app.get('/messages', (req, res)=>{
-	let allUsers = db.User.findAll();
-	let username = req.user.username;
-	let userID = req.user.id;
-
-
-	
-	if(!req.user){
-		res.render('login');
-	} else{
-		res.render('messages', { 
-			pets: req.pets,
-			allUsers: allUsers,
-			username: username,
-			userID: userID
-
-		});
-	}
+  if (!req.user) {
+    res.render("login");
+  } else {
+    res.render("messages", {
+      pets: req.pets,
+      allUsers: allUsers,
+      username: username,
+      userID: userID,
+      profileImg: profileImg,
+    });
+  }
 });
 
 // let myProfile;
 // module.exports={myProfile}
-app.get('/profile/:id', async (req, res)=>{
-	let userID = req.user.id;
-	// let newUserID = targetUser.id;
+app.get("/profile/:id", async (req, res) => {
+  let userID = req.user.id;
+  let profileImg = req.user.link;
 
-    if(!req.user){
-		res.render('error');
-	} else {
-		const targetUser = await db.User.findOne({
-			where: {
-				id: req.params.id
-			}
-		});
-		
-		const targetUserPets = await db.Pet.findAll({
-			where: {
-				UserId: targetUser.id
-			}
-		});
-		
-		res.render('profile', {
-			user: targetUser,
-			pets: targetUserPets,
-			numPets: req.user.numPets,
-			username: req.user.username,
-			userID: userID,
-			// newUserID: newUserID
-		});
-	}
-});
+  // let newUserID = targetUser.id;
 
+  if (!req.user) {
+    res.render("error");
+  } else {
+    const targetUser = await db.User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-//6-day weather forecast
-app.get('/weather', function(req, res){
-	let allUsers = db.User.findAll();
-	let username = req.user.username;
-	let userID = req.user.id;
+    const targetUserPets = await db.Pet.findAll({
+      where: {
+        UserId: targetUser.id,
+      },
+    });
 
-
-	
-	if(!req.user){
-		res.render('login');
-	} else{
-		res.render('weather', { 
-			pets: req.pets,
-			allUsers: allUsers,
-			username: username,
-			userID: userID,
-			numPets: req.user.numPets
-
-		});
-	}
+    res.render("profile", {
+      user: targetUser,
+      pets: targetUserPets,
+      numPets: req.user.numPets,
+      username: req.user.username,
+      userID: userID,
+      profileImg: profileImg,
+      // newUserID: newUserID
+    });
+  }
 });
 
 // Register page
-app.get('/register', function(req, res){
-	if(!req.user){
-		res.render('register');
-	} else {
-		res.redirect('/home');
-	}
+app.get("/register", function (req, res) {
+  if (!req.user) {
+    res.render("register");
+  } else {
+    res.redirect("/home");
+  }
+});
+
+//6-day weather forecast
+app.get("/weather", function (req, res) {
+  let allUsers = db.User.findAll();
+  let username = req.user.username;
+  let userID = req.user.id;
+
+  if (!req.user) {
+    res.render("login");
+  } else {
+    res.render("weather", {
+      pets: req.pets,
+      allUsers: allUsers,
+      username: username,
+      userID: userID,
+      numPets: req.user.numPets,
+    });
+  }
 });
 
 // Socket Route
-app.get('/sms', async (req, res) => {
-	// let allUsers = db.User.findAll();
-	// let username = req.user.username;
-	res.sendFile(path.join(clientDir, '../client/assets/index.html'))
-	
-	// res.render('test.pug', { 
-	
-	// })
+app.get("/sms", async (req, res) => {
+  // let allUsers = db.User.findAll();
+  // let username = req.user.username;
+  res.sendFile(path.join(clientDir, "../client/assets/index.html"));
+
+  // res.render('test.pug', {
+
+  // })
 });
 
 // Server Init
 db.sequelize.sync().then(() => {
-	http.listen(PORT, function () {
-		console.log("App now listening at localhost:" + PORT);
-	});
+  http.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+  });
 });
 
 //added bottom
 //calling socket.js imported file and route
-require("./controllers/msg-controller")(app)
+require("./controllers/msg-controller")(app);
 require("./socket")(io);
